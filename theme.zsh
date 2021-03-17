71f62bd2df19e54901033c504ea58adff2f2a64e
Alexandru Dinu <alex.dinu07@gmail.com>
2021-03-17 21:10:19 +0200

    Use different colors for git dirty; looks better.

--- theme.zsh
+++ theme.zsh
@@ -1,12 +1,15 @@
-COLOR_MAIN="152"
-COLOR_ERROR="009"
+COLOR_MAIN="159"
 COLOR_INFO="007"
+COLOR_DIRTY="167"
 
 function _git_info() {
     local br="$(git_current_branch)"
     if [[ -n $br ]]; then
-        [[ $(git_is_dirty) = "true" ]] && br+="*"
-        echo "%F{$COLOR_INFO}$br%f"
+        if [[ $(git_is_dirty) = "true" ]]; then
+            echo "%F{$COLOR_DIRTY}$br%f"
+        else
+            echo "%F{$COLOR_INFO}$br%f"
+        fi
     fi
 }
 
@@ -22,4 +25,4 @@ PROMPT+='%F{$COLOR_MAIN}%3~%f '
 [ $ENABLE_GIT_INFO = "true" ] && PROMPT+='$(_git_info) '
 [ $ENABLE_CONDA_INFO = "true" ] && PROMPT+='$(_conda_info) '
 PROMPT+=$'\n'
-PROMPT+='%(?:%F{$COLOR_MAIN}:%F{$COLOR_ERROR})${PROMPT_ARROW:-❯}%f '
+PROMPT+='%(?:%F{$COLOR_MAIN}:%F{$COLOR_DIRTY})${PROMPT_ARROW:-❯}%f '
