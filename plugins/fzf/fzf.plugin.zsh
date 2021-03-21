377bdbc1c03c800d7362b1f6875c2f843a0e3875
Alexandru Dinu <alex.dinu07@gmail.com>
2021-03-21 19:07:12 +0200

    (#10) Enable unattended install with good defaults for zsh.

--- plugins/fzf/fzf.plugin.zsh
+++ plugins/fzf/fzf.plugin.zsh
@@ -4,7 +4,7 @@ FZF_ZSH=$HOME/.fzf.zsh
 if [[ ! -d $FZF_ROOT_DIR ]]; then
     echo "fzf is not installed in $FZF_ROOT_DIR. Installing..."
     git clone --depth=1 https://github.com/junegunn/fzf.git $FZF_ROOT_DIR
-    $FZF_ROOT_DIR/install
+    $FZF_ROOT_DIR/install --key-bindings --completion --no-update-rc --no-bash --no-fish
 fi
 
 export FZF_DEFAULT_OPS="--extended"
