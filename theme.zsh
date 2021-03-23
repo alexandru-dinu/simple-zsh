f1e34c05b64ed7d549d3b35603ee6842f9489a5f
Alexandru Dinu <alex.dinu07@gmail.com>
2021-03-23 11:21:12 +0200

    Fix spacing for git and conda prompt info.

--- theme.zsh
+++ theme.zsh
@@ -8,20 +8,20 @@ function _git_info() {
         if [[ $(git_is_dirty) = "true" ]]; then
             br+="%F{$ZSH_COLOR_DIRTY}*%f"
         fi
-        echo "%F{$ZSH_COLOR_INFO}$br%f"
+        echo "%F{$ZSH_COLOR_INFO}$br%f "
     fi
 }
 
 function _conda_info() {
     # only show info on non-base envs
     if [[ -n CONDA_DEFAULT_ENV && $CONDA_DEFAULT_ENV != "base" ]]; then
-        echo "%F{$ZSH_COLOR_INFO}$CONDA_DEFAULT_ENV%f"
+        echo "%F{$ZSH_COLOR_INFO}$CONDA_DEFAULT_ENV%f "
     fi
 }
 
 PROMPT=$'\n'
 PROMPT+='%F{$ZSH_COLOR_MAIN}%3~%f '
-[ $ZSH_ENABLE_GIT_INFO = "true" ] && PROMPT+='$(_git_info) '
-[ $ZSH_ENABLE_CONDA_INFO = "true" ] && PROMPT+='$(_conda_info) '
+[ $ZSH_ENABLE_GIT_INFO = "true" ] && PROMPT+='$(_git_info)'
+[ $ZSH_ENABLE_CONDA_INFO = "true" ] && PROMPT+='$(_conda_info)'
 PROMPT+=$'\n'
 PROMPT+='%(?:%F{$ZSH_COLOR_MAIN}:%F{$ZSH_COLOR_DIRTY})${ZSH_PROMPT_ARROW:-❯}%f '
